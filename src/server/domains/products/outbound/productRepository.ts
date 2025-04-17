6fb2bae2cc75f8a849011accda03a50b4961669b
rockabetty <marissafromtexas@gmail.com>
2025-04-17 14:07:12 -0700

    removing double-table approach for simplicity

--- src/server/domains/products/outbound/productRepository.ts
+++ src/server/domains/products/outbound/productRepository.ts
@@ -1,7 +1,7 @@
 import { handleDatabaseError } from "@errors";
 import { queryDbConnection, withTransaction } from "@postgres";
 
-export async function insertOrSelectOneProductTemplate (templateData) {
+export async function insertOrSelectOneProduct (templateData) {
   return withTransaction(async (client) => {
     const insertQuery=`
     INSERT INTO product_templates
@@ -11,7 +11,8 @@ export async function insertOrSelectOneProductTemplate (templateData) {
       unit_id,
       package_count,
       display_quantity,
-      packaged_item
+      packaged_item,
+      brand_id,
     )
     VALUES
     (
@@ -20,7 +21,8 @@ export async function insertOrSelectOneProductTemplate (templateData) {
       $3,
       $4,
       $5,
-      $6
+      $6,
+      $7
     )
     ON CONFLICT ON CONSTRAINT unique_product_templates DO NOTHING
     RETURNING id
@@ -32,7 +34,8 @@ export async function insertOrSelectOneProductTemplate (templateData) {
       unit_id,
       package_count,
       display_quantity,
-      packaged_item
+      packaged_item,
+      brand_id
     } = templateData;
 
     const values=[
@@ -41,14 +44,15 @@ export async function insertOrSelectOneProductTemplate (templateData) {
       unit_id,
       package_count,
       display_quantity,
-      packaged_item
+      packaged_item,
+      brand_id
     ];
 
     try {
-        let newProductTemplate = await queryDbConnection(insertQuery, values);
+        let newProduct = await queryDbConnection(insertQuery, values);
         // if nothing was returned then it's possible it's due to the DO NOTHING
         // so let's double check. COALESCE is to account for potential null values
-        if (newProductTemplate.rows.length == 0) {
+        if (newProduct.rows.length == 0) {
           const selectQuery = `
             SELECT id
             FROM product_templates
@@ -60,56 +64,13 @@ export async function insertOrSelectOneProductTemplate (templateData) {
             AND COALESCE(display_quantity, -1) = COALESCE($5, -1)
             AND packaged_item = $6
           `;
-          newProductTemplate = await queryDbConnection(selectQuery, values);
+          newProduct = await queryDbConnection(selectQuery, values);
         }
 
-        return newProductTemplate.rows[0]
+        return newProduct.rows[0]
         // Todo: specific error handling, e.g. tunique constraint violation
     } catch (error) {
         handleDatabaseError(error);
     }
   });
-}
-
-export async function insertOrSelectOneProduct(productData) {
-    return withTransaction(async(client) => {
-        const insertQuery = `INSERT INTO products
-        (
-            product_template_id,
-            brand_id
-        )
-        VALUES 
-        (
-            $1,
-            $2
-        )
-        ON CONFLICT ON
-            (product_template_id, brand_id)
-            DO NOTHING
-        RETURNING id
-        `;
-
-        const values = [
-            productData.product_template_id,
-            productData.brand_id
-        ];
-
-        try {
-            const newProduct = await queryDbConnection(insertQuery, values);
-            if (newProduct.rows.length != 0) {
-                return newProduct.rows[0].id;
-            }
-            const selectQuery = `
-                SELECT id
-                FROM products
-                WHERE
-                    product_template_id = $1 AND
-                    brand_id = $2`;
-
-            const selectedProduct = await queryDbConnection(selectQuery, values);
-            return selectedProduct.rows[0].id;
-        } catch (error) {
-            handleDatabaseError(error);
-        }
-    });
 };
