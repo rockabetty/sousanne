71be3acd374d824edf6ccd0093a7e8baea987d47
rockabetty <marissafromtexas@gmail.com>
2025-04-17 20:09:31 -0700

    Adding units model to types

--- src/server/domains/units/units.types.ts
+++ src/server/domains/units/units.types.ts
@@ -1,6 +1,17 @@
-export type Unit = {
-    id?: number;
-    name?: string;
+export type UnitModel = {
+    id: number;
+    name: string;
     is_volume?: boolean;
-    abbreviaton?: string
+    abbreviation?: string;
 }
+
+export type UnitModelColumn = keyof UnitModel;
+
+export const unitModelColumns: UnitModelColumn[] = [
+    "id",
+    "name",
+    "is_volume",
+    "abbreviation"
+];
+
+export const unitModelColumnSet: Set<UnitModelColumn> = new Set(unitModelColumns);
