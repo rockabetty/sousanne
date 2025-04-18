e4f8adfb804abd9e5c4564c6c9ca66d4e644063f
rockabetty <marissafromtexas@gmail.com>
2025-04-17 20:14:09 -0700

    Products types definitions

---
+++ src/pages/products/products.types.ts
@@ -0,0 +1,31 @@
+export type ProductModel = {
+    id: number;
+    name: string;
+    ingredient_id: number;
+    created_at?: Date;
+    unit_id: number;
+    base_quantity?: number;
+    packaged_item?: boolean;
+    package_count?: number;
+    display_quantity?: number;
+    brand_id?: number;
+    product_template_id?: number;
+}
+
+export type ProductModelColumn = keyof ProductModel;
+
+export const productModelColumns: ProductModelColumn[] = [
+    "id",
+    "name",
+    "ingredient_id",
+    "created_at",
+    "unit_id",
+    "base_quantity",
+    "packaged_item",
+    "package_count",
+    "display_quantity",
+    "brand_id",
+    "product_template_id"
+];
+
+export const productModelColumnSet: Set<ProductModelColumn> = new Set(productModelColumns);
