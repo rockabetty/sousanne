c22f437ac6ab56e6bf1b91335a2cca21ea7138a6
rockabetty <marissafromtexas@gmail.com>
2025-04-17 20:10:00 -0700

    adding recipes model to types

--- src/server/domains/recipes/recipes.types.ts
+++ src/server/domains/recipes/recipes.types.ts
@@ -1,13 +1,33 @@
-export type Recipe = {
-    id?: number;
-    name?: string;
+export type RecipeModel = {
+    id: number;
+    name: string;
+    slug: string;
     user_id?: number;
     is_public?: boolean;
     cuisine_id?: number;
     base_serving_size?: number;
+    wait_time?: number;
+    active_prep_time?: number;
     cook_time?: number;
     oven_preheat?: number;
     rating?: number;
-    active_prep_time?: number;
-    wait_time?: number;
 }
+
+export type RecipeModelColumn = keyof RecipeModel;
+
+export const recipeModelColumns: RecipeModelColumn[] = [
+    "id",
+    "name",
+    "slug",
+    "user_id",
+    "is_public",
+    "cuisine_id",
+    "base_serving_size",
+    "wait_time",
+    "active_prep_time",
+    "cook_time",
+    "oven_preheat",
+    "rating"
+];
+
+export const recipeModelColumnSet: Set<RecipeModelColumn> = new Set(recipeModelColumns);
