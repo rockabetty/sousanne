248e726ee633f0a69e456ab507e894739d5a93d5
rockabetty <marissafromtexas@gmail.com>
2025-04-19 18:23:44 -0700

    Making the index page look not like hammered dogshit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,21 @@
-import axios from 'axios';
-import slugify from 'slugify'
-import Link from 'next/link'
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
-    const response = await axios.get(`${NEXT_PUBLIC_API_URL}/api/recipes`);
-    
-    const recipes = response.data.map(recipe => ({
-      ...recipe,
-      slug: slugify(recipe.name, { lower: true })
-    }));
-
-    return {
-      props: {
-        recipes: recipes || []
-      },
-    };
-
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        recipes: ["error!"]
-      },
-    };
-  }
-};
-
-interface RecipesProps {
-  recipes: any[];
-};
-
-const Recipes: NextPage<RecipesProps> = ({ recipes }) => {
+import axios from 'axios'
+import SousanneLayout from '@components/layout/SousannePage'
+import { Link } from 'el-cuc-ui'
+const MainPage: NextPage<RecipesProps> = () => {
   return (
-    <div>
-      <h1>Recipes</h1>
-      <ul>
-      {recipes?.map(recipe => (
-        <li key={recipe.id}><Link href={`/recipe/${recipe.slug}`}>{recipe.name}</Link></li>
-      ))}
-      </ul>
-    </div>
+    <SousanneLayout>
+      <main>
+        <h1>Sousanne</h1>
+        <Link type="navigation">Price Book</Link>
+        <Link type="navigation" href="/recipes">
+          Recipes
+        </Link>
+        <Link href="/ingredients" type="navigation">
+          Ingredients
+        </Link>
+      </main>
+    </SousanneLayout>
   )
-};
+}
 
-export default Recipes;
+export default MainPage
